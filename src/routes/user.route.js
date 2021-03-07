87e9bdcb3727d40dabde0e164ccb0e7963855831
Leo Peyre <leo.peyre95@gmail.com>
2021-03-07 22:47:21 +0100

    cors, token

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 const user = require('../controllers/users.controller');
+const verifyToken = require('../middlewares/verifyToken');
 
 router.post('/users', user.create);
 router.post('/users/login', user.login);
-router.get('/users/:id', user.findOne);
 
+//import du middleware pour la verification de token
+router.get('/users/:id', verifyToken, user.findOne);
 
 module.exports = router;
--- src/services/express.service.js
+++ src/services/express.service.js
@@ -3,12 +3,14 @@ const express =  require('express');
 const app =  express();
 const apiRouter = require('../routes')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 exports.start = ( ) => {
 
     const port = process.env.PORT;
 
     app.use(bodyParser.json());
+    app.use(cors());
     app.use('/api/v1', apiRouter);
 
     app.listen(port, (err) => {
